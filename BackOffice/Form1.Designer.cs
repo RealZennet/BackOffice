837fc4c9a06bfdfe571ae4c039fe8fcfdc165f4d
damian suffo <suffodamian02@gmail.com>
2023-06-28 04:29:13 -0300

    estructura de colores basica

--- BackOffice/Form1.Designer.cs
+++ BackOffice/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BackOffice
 {
-    partial class Form1
+    partial class QuickCarry
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SlidePanel = new System.Windows.Forms.Panel();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonMinimize = new System.Windows.Forms.Button();
+            this.buttonMaximize = new System.Windows.Forms.Button();
+            this.SlidePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SlidePanel
+            // 
+            this.SlidePanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.SlidePanel.Controls.Add(this.buttonMaximize);
+            this.SlidePanel.Controls.Add(this.buttonMinimize);
+            this.SlidePanel.Controls.Add(this.buttonClose);
+            this.SlidePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SlidePanel.Location = new System.Drawing.Point(0, 0);
+            this.SlidePanel.Name = "SlidePanel";
+            this.SlidePanel.Size = new System.Drawing.Size(800, 41);
+            this.SlidePanel.TabIndex = 0;
+            this.SlidePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SlidePanel_MouseDown);
+            this.SlidePanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SlidePanel_MouseMove);
+            this.SlidePanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SlidePanel_MouseUp);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Location = new System.Drawing.Point(746, 3);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(42, 29);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonMinimize
+            // 
+            this.buttonMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimize.Location = new System.Drawing.Point(647, 3);
+            this.buttonMinimize.Name = "buttonMinimize";
+            this.buttonMinimize.Size = new System.Drawing.Size(42, 29);
+            this.buttonMinimize.TabIndex = 1;
+            this.buttonMinimize.Text = "_";
+            this.buttonMinimize.UseVisualStyleBackColor = true;
+            this.buttonMinimize.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // buttonMaximize
+            // 
+            this.buttonMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMaximize.Location = new System.Drawing.Point(698, 3);
+            this.buttonMaximize.Name = "buttonMaximize";
+            this.buttonMaximize.Size = new System.Drawing.Size(42, 29);
+            this.buttonMaximize.TabIndex = 2;
+            this.buttonMaximize.Text = "▢";
+            this.buttonMaximize.UseVisualStyleBackColor = true;
+            this.buttonMaximize.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // QuickCarry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.SlidePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "QuickCarry";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SlidePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel SlidePanel;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonMinimize;
+        private System.Windows.Forms.Button buttonMaximize;
     }
 }
 
--- BackOffice/Form1.cs
+++ BackOffice/Form1.cs
@@ -10,11 +10,65 @@ using System.Windows.Forms;
 
 namespace BackOffice
 {
-    public partial class Form1 : Form
+    public partial class QuickCarry : Form
     {
-        public Form1()
+        public QuickCarry()
         {
             InitializeComponent();
+            this.BackColor = Color.FromArgb(22, 75, 96);
+            this.SlidePanel.BackColor = Color.FromArgb(79, 192, 208);
+            this.buttonClose.BackColor = Color.FromArgb(27, 107, 147);
+            this.buttonMaximize.BackColor = Color.FromArgb(27, 107, 147);
+            this.buttonMinimize.BackColor = Color.FromArgb(27, 107, 147);
         }
+
+        //Variables para controlar la posicion del slide
+        public int m, x, y;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+             [BUG] - > No reescala items del form.
+             this.WindowState = FormWindowState.Minimized;
+
+             */
+        }
+
+        private void SlidePanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            m = 1;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void SlidePanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - x, MousePosition.Y - y);
+            }
+        }
+
+        private void SlidePanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            m = 0;
+        }
+
+
     }
 }
