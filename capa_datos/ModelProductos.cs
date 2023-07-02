6891401f1e9e5750a3fc24795bed5ba0099dc2bb
Damian suffo <suffodamian02@gmail.com>
2023-07-02 17:58:16 -0300

    metodo para guardar datos

---
+++ capa_datos/ModelProductos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_datos
+{
+    class ModelProductos:DataBaseControl
+    {
+        public string NombreProducto { get; set; }
+        public int PesoProducto { get; set; }
+        public int CantidadProducto { get; set; }
+        public string DescripcionProducto { get; set; }
+
+        public void Save()
+        {
+            this.Command.CommandText = $"INSERT INTO producto(nombre, precio) VALUES(" +
+                $"'{this.NombreProducto}', " +
+                $"{this.PesoProducto}, " +
+                $"{this.CantidadProducto}, " +
+                $"'{this.DescripcionProducto}')";
+            this.Command.ExecuteNonQuery();
+        }
+    }
+}
