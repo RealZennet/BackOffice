dd02bdc3d5d8a4743d449e025efc75b32bfe2855
Damian suffo <suffodamian02@gmail.com>
2023-07-02 18:08:29 -0300

    listar y eliminar productos

--- capa_datos/ModelProductos.cs
+++ capa_datos/ModelProductos.cs
@@ -8,6 +8,7 @@ namespace capa_datos
 {
     class ModelProductos:DataBaseControl
     {
+        public int Id { get; set; }
         public string NombreProducto { get; set; }
         public int PesoProducto { get; set; }
         public int CantidadProducto { get; set; }
@@ -22,5 +23,30 @@ namespace capa_datos
                 $"'{this.DescripcionProducto}')";
             this.Command.ExecuteNonQuery();
         }
+
+        public List<ModelProductos> TodosLosItems()
+        {
+            this.Command.CommandText = "SELECT * FROM producto";
+            this.Reader = this.Command.ExecuteReader();
+
+            List<ModelProductos> result = new List<ModelProductos>();
+            while (this.Reader.Read())
+            {
+                ModelProductos producto = new ModelProductos();
+                producto.Id = Int32.Parse(this.Reader["id_Prod"].ToString());
+                producto.NombreProducto = this.Reader["nom_Prod"].ToString();
+                producto.PesoProducto = Int32.Parse(this.Reader["peso_Prod"].ToString());
+                producto.CantidadProducto = Int32.Parse(this.Reader["cant_Prod"].ToString());
+                producto.DescripcionProducto = this.Reader["desc_Prod"].ToString();
+                result.Add(producto);
+            }
+            return result;
+        }
+        public void Delete()
+        {
+            this.Command.CommandText = $"DELETE FROM producto WHERE id = {this.Id}";
+            this.Command.ExecuteNonQuery();
+        }
+
     }
 }
