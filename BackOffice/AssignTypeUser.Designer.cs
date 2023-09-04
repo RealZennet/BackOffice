0a2dd1576c7f38da7d609700595c0ee9780b6fdc
Damian suffo <suffodamian02@gmail.com>
2023-09-04 00:04:34 -0300

    apartado camionero

--- BackOffice/AssignTypeUser.Designer.cs
+++ BackOffice/AssignTypeUser.Designer.cs
@@ -28,7 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonAddTrucker = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxUsernameTrucker = new System.Windows.Forms.TextBox();
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxUsernameOperator = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(17, 12);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(89, 26);
+            this.buttonBack.TabIndex = 36;
+            this.buttonBack.Text = "Volver";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonAddTrucker
+            // 
+            this.buttonAddTrucker.Location = new System.Drawing.Point(19, 435);
+            this.buttonAddTrucker.Name = "buttonAddTrucker";
+            this.buttonAddTrucker.Size = new System.Drawing.Size(71, 26);
+            this.buttonAddTrucker.TabIndex = 35;
+            this.buttonAddTrucker.Text = "Agregar";
+            this.buttonAddTrucker.UseVisualStyleBackColor = true;
+            this.buttonAddTrucker.Click += new System.EventHandler(this.buttonAddTrucker_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(114, 435);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(71, 26);
+            this.buttonDelete.TabIndex = 34;
+            this.buttonDelete.Text = "Eliminar";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(236, 435);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(166, 26);
+            this.buttonRefresh.TabIndex = 33;
+            this.buttonRefresh.Text = "Recargar";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Username:";
+            // 
+            // txtBoxUsernameTrucker
+            // 
+            this.txtBoxUsernameTrucker.Location = new System.Drawing.Point(17, 398);
+            this.txtBoxUsernameTrucker.Name = "txtBoxUsernameTrucker";
+            this.txtBoxUsernameTrucker.Size = new System.Drawing.Size(233, 20);
+            this.txtBoxUsernameTrucker.TabIndex = 31;
+            // 
+            // dataGridViewUsers
+            // 
+            this.dataGridViewUsers.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(75)))), ((int)(((byte)(96)))));
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Location = new System.Drawing.Point(17, 44);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.Size = new System.Drawing.Size(233, 322);
+            this.dataGridViewUsers.TabIndex = 30;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(75)))), ((int)(((byte)(96)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(461, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(241, 322);
+            this.dataGridView1.TabIndex = 37;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 435);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 26);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(556, 435);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 26);
+            this.button2.TabIndex = 40;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "Username:";
+            // 
+            // txtBoxUsernameOperator
+            // 
+            this.txtBoxUsernameOperator.Location = new System.Drawing.Point(459, 398);
+            this.txtBoxUsernameOperator.Name = "txtBoxUsernameOperator";
+            this.txtBoxUsernameOperator.Size = new System.Drawing.Size(243, 20);
+            this.txtBoxUsernameOperator.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(112, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "Camionero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.Location = new System.Drawing.Point(552, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 43;
+            this.label4.Text = "Operador";
             // 
             // AssignTypeUser
             // 
@@ -36,13 +179,46 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(75)))), ((int)(((byte)(96)))));
             this.ClientSize = new System.Drawing.Size(773, 559);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxUsernameOperator);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonAddTrucker);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxUsernameTrucker);
+            this.Controls.Add(this.dataGridViewUsers);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AssignTypeUser";
             this.Text = "AssignTypeUser";
+            this.Load += new System.EventHandler(this.AssignTypeUser_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonAddTrucker;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxUsernameTrucker;
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxUsernameOperator;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- BackOffice/AssignTypeUser.cs
+++ BackOffice/AssignTypeUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using capa_logica;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,47 @@ namespace BackOffice
         public AssignTypeUser()
         {
             InitializeComponent();
+        }
+
+        private void AssignTypeUser_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void RefreshTable()
+        {
+            DataTable dataTableUsersAssign = AssignTypeOfUserTruckerController.Obtener();
+            dataGridViewUsers.DataSource = dataTableUsersAssign;
+        }
+
+        public void ClearTxtBoxes()
+        {
+            txtBoxUsernameTrucker.Clear();
+            txtBoxUsernameOperator.Clear();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshTable();
+        }
+
+        private void addUserTracker()
+        {
+            AssignTypeOfUserTruckerController.Crear(txtBoxUsernameTrucker.Text);
+            MessageBox.Show("Usuario asignado a camionero");
+            RefreshTable();
+            ClearTxtBoxes();
+        }
+
+        private void buttonAddTrucker_Click(object sender, EventArgs e)
+        {
+            addUserTracker();
+
         }
     }
 }
