69e2911f38e84699673a7add8b20d84b25fc9760
Damian suffo <suffodamian02@gmail.com>
2023-09-04 00:10:56 -0300

    model de asignacion de operario

---
+++ capa_datos/AssignTypeOfUserOperatorModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_datos
+{
+    public class AssignTypeOfUserOperatorModel : DataBaseControl
+    {
+
+        public string UserName { get; set; }
+
+        public void Save()
+        {
+            this.Command.CommandText = $"INSERT INTO operario (username) VALUES (" +
+                $"'{this.UserName}')";
+            this.Command.ExecuteNonQuery();
+        }
+
+        public List<AssignTypeOfUserOperatorModel> GetAllOperatorsUsers()
+        {
+            this.Command.CommandText = $"SELECT * FROM operario";
+            this.Reader = this.Command.ExecuteReader();
+
+            List<AssignTypeOfUserOperatorModel> result = new List<AssignTypeOfUserOperatorModel>();
+            while (this.Reader.Read())
+            {
+                AssignTypeOfUserOperatorModel user = new AssignTypeOfUserOperatorModel();
+                user.UserName = this.Reader["username"].ToString();
+                result.Add(user);
+            }
+            return result;
+        }
+
+
+        public void DeleteUser()
+        {
+            this.Command.CommandText = $"DELETE FROM operario where username = '{this.UserName}'";
+            this.Command.ExecuteNonQuery();
+        }
+    }
+}
