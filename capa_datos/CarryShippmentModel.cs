b93df08c2763aaa8fb106a0d697ddf87b405bbe2
Damian suffo <suffodamian02@gmail.com>
2023-09-12 16:37:35 -0300

    model de transporte de cargas

---
+++ capa_datos/CarryShippmentModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_datos
+{
+    public class CarryShippmentModel : DataBaseControl
+    {
+        public int IDTruck { get; set; }
+        public int IDBatch { get; set; }
+        public int IDDestination { get; set; }
+        public enum ShippingStatus
+        {
+            Entregado,
+            EnCamino,
+            Retrasado,
+            NoEnviado
+        }
+        public ShippingStatus Status { get; set; }
+
+
+        public void Save()
+        {
+            string statusAsString = Enum.GetName(typeof(ShippingStatus), this.Status);
+            this.Command.CommandText = $"INSERT INTO transporta(id_camion, id_lote, id_des, estatus) VALUES(" +
+               $"{this.IDTruck}, " +
+               $"{this.IDBatch}, " +
+               $"{this.IDDestination}," +
+               $"'{statusAsString}')";
+            this.Command.ExecuteNonQuery();
+        }
+
+
+        public List<CarryShippmentModel> GetAllDestinations()
+        {
+            this.Command.CommandText = "SELECT * FROM transporta";
+            this.Reader = this.Command.ExecuteReader();
+
+            List<CarryShippmentModel> result = new List<CarryShippmentModel>();
+            while (this.Reader.Read())
+            {
+                CarryShippmentModel destinations = new CarryShippmentModel();
+                destinations.IDTruck = Int32.Parse(this.Reader["id_camion"].ToString());
+                destinations.IDBatch = Int32.Parse(this.Reader["id_lote"].ToString());
+                destinations.IDDestination = Int32.Parse(this.Reader["fech_sal"].ToString());
+                string statusAsString = this.Reader["estatus"].ToString();
+                if (Enum.TryParse(statusAsString, out ShippingStatus status))
+                {
+                    destinations.Status = status;
+                }
+                else
+                {
+                    destinations.Status = ShippingStatus.NoEnviado;
+                }
+                result.Add(destinations);
+            }
+            this.Reader.Close();
+            return result;
+        }
+        public void Delete()
+        {
+            this.Command.CommandText = $"DELETE FROM transporta WHERE id_camion = {this.IDTruck}";
+            this.Command.ExecuteNonQuery();
+        }
+    }
+}
+
