8d7317d386fd4b013c237399fd9a9f868058c3db
Damian suffo <suffodamian02@gmail.com>
2023-09-11 23:30:54 -0300

    model de almacen

---
+++ capa_datos/StoreHouseModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_datos
+{
+    public class StoreHouseModel: DataBaseControl
+    {
+        public int IDStoreHouse { get; set; }
+        public bool ActivatedStoreHouse { get; set; }
+        public string Street { get; set; }
+        public int DoorNumber { get; set; }
+        public string Corner { get; set; }
+
+        public void Save()
+        {
+            this.Command.CommandText = $"INSERT INTO almacen(calle, num, esq, bajalogica) VALUES(" +
+               $"'{this.Street}', " +
+               $"{this.DoorNumber}, " +
+               $"'{this.Corner}'," +
+               $"'{this.ActivatedStoreHouse}')";
+            this.Command.ExecuteNonQuery();
+        }
+
+        public List<StoreHouseModel> GetAllStoreHouse()
+        {
+            this.Command.CommandText = "SELECT * FROM almacen";
+            this.Reader = this.Command.ExecuteReader();
+
+            List<StoreHouseModel> result = new List<StoreHouseModel>();
+            while (this.Reader.Read())
+            {
+                StoreHouseModel storehouse = new StoreHouseModel();
+                storehouse.IDStoreHouse = Int32.Parse(this.Reader["id_alma"].ToString());
+                storehouse.Street = this.Reader["calle"].ToString();
+                storehouse.DoorNumber = Int32.Parse(this.Reader["num"].ToString());
+                storehouse.Corner = this.Reader["esq"].ToString();
+                storehouse.ActivatedStoreHouse = Convert.ToBoolean(this.Reader["bajalogica"].ToString());
+                result.Add(storehouse);
+            }
+            this.Reader.Close();
+            return result;
+        }
+        public void Delete()
+        {
+            this.Command.CommandText = $"DELETE FROM almacen WHERE id_alma = {this.IDStoreHouse}";
+            this.Command.ExecuteNonQuery();
+        }
+    }
+}
