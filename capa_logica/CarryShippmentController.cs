b94ee9b562342d1f75a882366cd818ad0e0580a7
Damian suffo <suffodamian02@gmail.com>
2023-09-12 16:50:58 -0300

    controlador de transporte de cargas

---
+++ capa_logica/CarryShippmentController.cs
@@ -0,0 +1,54 @@
+﻿using capa_datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static capa_datos.CarryShippmentModel;
+
+namespace capa_logica
+{
+    public abstract class CarryShippmentController
+    {
+        public static void Create(int idtruck, int idbatch, int iddestination, ShippingStatus status)
+        {
+            CarryShippmentModel carry = new CarryShippmentModel();
+            carry.IDTruck = idtruck;
+            carry.IDBatch = idbatch;
+            carry.IDDestination = iddestination;
+            carry.Status = status;
+            carry.Save();
+        }
+
+
+        public static DataTable GetAllCarrys()
+        {
+            CarryShippmentModel CarrysTableModel = new CarryShippmentModel();
+            List<CarryShippmentModel> carries = CarrysTableModel.GetAllDestinations();
+            DataTable table = new DataTable();
+            table.Columns.Add("ID Camion", typeof(int));
+            table.Columns.Add("ID Lote", typeof(int));
+            table.Columns.Add("ID Destino", typeof(int));
+            table.Columns.Add("Status", typeof(ShippingStatus));
+
+
+            foreach (CarryShippmentModel carry in carries)
+            {
+                DataRow row = table.NewRow();
+                row["ID Camion"] = carry.IDTruck;
+                row["ID Lote"] = carry.IDBatch;
+                row["ID Destino"] = carry.IDDestination;
+                row["Status"] = carry.Status.ToString(); 
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+        public static void DeleteCarry(int id)
+        {
+            CarryShippmentModel Carry = new CarryShippmentModel();
+            Carry.IDTruck = id;
+            Carry.Delete();
+        }
+    }
+}
