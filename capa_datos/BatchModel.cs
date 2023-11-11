9adb21d144ea0187561121344b8876cf32f366d3
Damian suffo <suffodamian02@gmail.com>
2023-11-11 06:45:03 -0300

    fix inyeccion sql de batchmodel

--- capa_datos/BatchModel.cs
+++ capa_datos/BatchModel.cs
@@ -19,23 +19,29 @@ namespace capa_datos
 
         public void Save()
         {
-            try {
-            this.Command.CommandText = $"INSERT INTO lote (email, fech_Crea, fech_Entre, id_Des, posicion, bajalogica) VALUES " +
-                $"('{this.Email.ToString()}', " +
-                $"'{this.DateOfCreation.ToString("yyyy-MM-dd HH:mm:ss")}', " +
-                $"'{this.ShippingDate.ToString("yyyy-MM-dd HH:mm:ss")}', " +
-                $"{this.IDShipp}, " +
-                $"'{this.Position}', " +
-                $"{this.ActivedBatch})";
-            this.Command.ExecuteNonQuery();
+            try
+            {
+                this.Command.CommandText = "INSERT INTO lote (email, fech_Crea, fech_Entre, id_Des, posicion, bajalogica) " +
+                                           "VALUES (@Email, @DateOfCreation, @ShippingDate, @IDShipp, @Position, @ActivedBatch)";
 
-            this.Command.CommandText = "SELECT last_insert_id()";
-            this.IDBatch = Convert.ToInt32(this.Command.ExecuteScalar());
-            }catch(Exception ex)
+                this.Command.Parameters.AddWithValue("@Email", this.Email.ToString());
+                this.Command.Parameters.AddWithValue("@DateOfCreation", this.DateOfCreation.ToString("yyyy-MM-dd HH:mm:ss"));
+                this.Command.Parameters.AddWithValue("@ShippingDate", this.ShippingDate.ToString("yyyy-MM-dd HH:mm:ss"));
+                this.Command.Parameters.AddWithValue("@IDShipp", this.IDShipp);
+                this.Command.Parameters.AddWithValue("@Position", this.Position);
+                this.Command.Parameters.AddWithValue("@ActivedBatch", this.ActivedBatch);
+
+                this.Command.ExecuteNonQuery();
+
+                this.Command.CommandText = "SELECT last_insert_id()";
+                this.IDBatch = Convert.ToInt32(this.Command.ExecuteScalar());
+            }
+            catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
         }
+
 
         public List<BatchModel> GetAllLots()
         {
